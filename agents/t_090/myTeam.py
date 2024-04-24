3470e0e0414c5b724368f55fa8aa14612a1c0ea0
Marshall Zhang <zijuzhang1@student.unimelb.edu.au>
2024-04-24 20:58:33 +1000

    Add SplendorState class to represent game state

--- agents/t_090/myTeam.py
+++ agents/t_090/myTeam.py
@@ -1,4 +1,26 @@
 from template import Agent
+from copy import deepcopy
+import random
+import math
+
+class SplendorState:
+    '''
+    Class to represent the state of the game at any point in time
+    '''
+    def __init__(self, gems, cards, scores, nobles, reserved_cards):
+        self.gems = gems  # Dictionary of available gems
+        self.cards = cards  # Cards available for purchase
+        self.scores = scores  # Dictionary of player scores
+        self.nobles = nobles  # Noble tiles available for claiming
+        self.reserved_cards = reserved_cards  # Cards reserved by players
+
+    def copy(self):
+        '''
+        Returns a deep copy of the current state
+        '''
+        return SplendorState(deepcopy(self.gems), deepcopy(self.cards), deepcopy(self.scores),
+                             deepcopy(self.nobles), deepcopy(self.reserved_cards))
+
 
 class myAgent(Agent):
     def __init__(self,_id):
@@ -6,3 +28,4 @@ class myAgent(Agent):
     
     def SelectAction(self, actions, game_state):
         return actions[0]
+    
