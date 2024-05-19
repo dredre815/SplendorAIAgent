b5cf9a76f91a1abe5accab1d86a91ca41b9503ac
Marshall Zhang <zijuzhang1@student.unimelb.edu.au>
2024-05-19 20:25:43 +1000

    Test on the server V5

--- agents/t_090/myTeam.py
+++ agents/t_090/myTeam.py
@@ -163,19 +163,11 @@ class MCTS:
         return diversity_score
     
     def improved_feature4(self, on_board, action_rewards):
-        # Get current gem counts, excluding the last type of gem
+        # Calculate the diversity score of gems
         gem_values = list(on_board["my_gem"].values())[:-1]
-        # Predict future gem card counts, excluding the last type of card reward
-        gem_card_value = np.array(list(on_board["my_gemcard"].values()))
-        card_re = np.array(list(action_rewards["gem_cards_rewards"].values())[1:])
-        future_cardgems = gem_card_value + card_re
-        # Calculate scores for each gem type when less than 4 gems are present
-        scores = [4 - gem for gem in gem_values]
-        total_score = sum(scores)
-        normalized_score = total_score / (sum(future_cardgems[:-1]) + 1)
-        last_gem_card_bonus = future_cardgems[-1] * 2
-        # Final diversity score
-        diversity_score = normalized_score + last_gem_card_bonus
+        gem_cards = np.array(list(on_board["my_gemcard"].values())) + np.array(list(action_rewards["gem_cards_rewards"].values())[1:])
+        diversity_score = sum([4 - i for i in gem_values ])/ (sum(gem_cards[:-1])+1)+gem_cards [-1]*2
+
         return diversity_score
 
     def feature5(self, on_board, action_rewards):
@@ -331,7 +323,7 @@ class MCTS:
             game_state = game_rule.generateSuccessor(game_state, action, next_agent_id)
 
             simulation_depth += 1
-            
+        
         return reward
 
     def Backpropagate(self, node, value):
@@ -491,7 +483,6 @@ class MCTS:
                 card_probabilities.append((gem_on_card + gem + yellow_gems) / count)
                 yellow_gems = 0
                
-        # Return the minimum probability
         return min(card_probabilities)
     
     def CheckUsefulCard(self, game_state):
